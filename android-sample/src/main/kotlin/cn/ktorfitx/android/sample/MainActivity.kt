318f094dd7674cbbb3e7407b63dbaefcc87dfc6a
李佳伟 <2965502228@qq.com>
2026-02-04 20:51:28 +0800

    fix: 修复 sample 刷新率和部分设备底部导航栏白色问题

--- android-sample/src/main/kotlin/cn/ktorfitx/android/sample/MainActivity.kt
+++ android-sample/src/main/kotlin/cn/ktorfitx/android/sample/MainActivity.kt
@@ -9,15 +9,15 @@ import androidx.activity.enableEdgeToEdge
 import cn.ktorfitx.multiplatform.sample.App
 
 class MainActivity : ComponentActivity() {
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		enableEdgeToEdge(
-			statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
-			navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
-		)
-		setContent {
-			App()
-		}
-	}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
+        setContent {
+            App()
+        }
+    }
 }
