f29d1cfd9d0f7fe00c9a00c1c54db5e53df741af
李佳伟 <2965502228@qq.com>
2026-02-12 21:26:53 +0800

    update: 3.4.0-3.3.1

--- gradle-plugin/common-gradle-plugin/src/main/kotlin/cn/ktorfitx/common/gradle/plugin/KtorfitxVersions.kt
+++ gradle-plugin/common-gradle-plugin/src/main/kotlin/cn/ktorfitx/common/gradle/plugin/KtorfitxVersions.kt
@@ -2,7 +2,7 @@ package cn.ktorfitx.common.gradle.plugin
 
 object KtorfitxVersions {
 
-    const val KTORFITX = "3.4.0-3.3.0"
+    const val KTORFITX = "3.4.0-3.3.1"
 
     const val KTOR = "3.4.0"
 }
