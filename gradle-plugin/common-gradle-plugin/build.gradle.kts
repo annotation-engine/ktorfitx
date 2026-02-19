c09e2a0083936da7005fc5c4d296902f9fa964ee
李佳伟 <2965502228@qq.com>
2026-02-20 00:58:27 +0800

    fix: 修复找不到 common-gradle-plugin 模块的问题

--- gradle-plugin/common-gradle-plugin/build.gradle.kts
+++ gradle-plugin/common-gradle-plugin/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     `kotlin-dsl`
-    `java-gradle-plugin`
+    alias(libs.plugins.maven.publish)
 }
 
 val ktorfitxVersion = property("ktorfitx.version").toString()
+val ktorfitxAutomaticRelease = property("ktorfitx.automaticRelease").toString().toBoolean()
+
 group = "cn.ktorfitx.common.gradle.plugin"
 version = ktorfitxVersion
 
@@ -13,4 +15,38 @@ kotlin {
 
 dependencies {
     implementation(libs.bundles.common.gradle.plugin)
+}
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = ktorfitxAutomaticRelease)
+    signAllPublications()
+
+    coordinates("cn.ktorfitx", "common-gradle-plugin", ktorfitxVersion)
+
+    pom {
+        name.set("common-gradle-plugin")
+        description.set("Ktorfitx 是一款专为 Ktor 设计的代码生成框架，致力于减少样板代码，为 Ktor Client 和 Ktor Server 提供代码生成服务，支持 Kotlin Multiplatform")
+        inceptionYear.set("2025")
+        url.set("https://github.com/annotation-engine/ktorfitx")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("JarvisLi")
+                name.set("JarvisLi")
+                url.set("https://github.com/annotation-engine/ktorfitx")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/annotation-engine/ktorfitx")
+            connection.set("scm:git:git://github.com/annotation-engine/ktorfitx.git")
+            developerConnection.set("scm:git:ssh://git@github.com:annotation-engine/ktorfitx.git")
+        }
+    }
 }
